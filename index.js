c7c59e6bf7cf8abf7aa71effcaa56bd43a390b37
Conrado Peloso <conradopeloso.cp@gmail.com>
2026-04-06 11:08:06 -0300

    update index.js and conexion.js

--- index.js
+++ index.js
@@ -22,20 +22,20 @@ app.use(cors()); // app.use permite configurar middlewares, en este caso cors
 app.use(express.json()); // app.use permite configurar middlewares, en este caso express.json() para convertir el body a un objeto JS
 app.use(express.urlencoded({extended:true})); // asi express recibe los datos en urlencode y los parsea a json
 
-// SERVIDOR Y PETICIONES HTTP
-app.listen(puerto, () => { //COLBACK que se ejecuta cuando el servidor está corriendo, en este caso para mostrar un mensaje en la consola
-    console.log(`Servidor corriendo en el puerto ${puerto}`);
-});
-
-
-//RUTAS
-const rutas_articulo = require("./routers/ArticuloRouter");
-const rutas_usuario  = require("./routers/UsuarioRouter");
-
 // CARPETA PUBLICA para servir imágenes estáticas
 // Esto permite acceder a los archivos de /uploads desde el navegador
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-//cargar las rutas
+// Definicion de rutas
+const rutas_articulo = require("./routers/ArticuloRouter");
+const rutas_usuario  = require("./routers/UsuarioRouter");
+
+// Carga de RUTAS
 app.use("/api", rutas_articulo);
 app.use("/api", rutas_usuario);
+
+
+// SERVIDOR Y PETICIONES HTTP
+app.listen(puerto, "0.0.0.0", () => { //COLBACK que se ejecuta cuando el servidor está corriendo, en este caso para mostrar un mensaje en la consola
+    console.log(`Servidor corriendo en el puerto ${puerto}`);
+});
